ec2946265e4a7c8801016aa42687edbb6b4b564c
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-05-11 03:20:33 +0530

    Added travel-log service

---
+++ travel-log/server.js
@@ -0,0 +1,46 @@
+"use strict";
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+// const routes = require("./api/routes");
+
+// Express App
+const app = express();
+// Setup DB Connection
+// require("./db");
+
+// Use default logger for now
+app.use(logger("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// This is to check if the service is online or not
+app.get("/ping", function(req, res) {
+  res.json({ reply: "pong" });
+  res.end();
+});
+
+// error handler
+app.use((error, req, res, next) => {
+    console.log(error);
+    return res.json({});
+});
+
+// Here you set the PORT and IP of the server
+const port = process.env.PORT || 8002;
+const ip = process.env.IP || "127.0.0.1";
+
+app.listen(port, ip, function(error) {
+  if (error) {
+    console.error("Unable to listen for connections", error);
+    throw error;
+  }
+  console.info(`Server on http://${ip}:${port}`);
+});
